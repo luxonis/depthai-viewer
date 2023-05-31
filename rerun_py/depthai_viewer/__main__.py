ea7390cb6fae53a2dc612a69a68093b60ddab3b4
zrezke <jeretina.filip@gmail.com>
2023-05-31 11:01:05 +0200

    Improved first time startup stability

--- rerun_py/depthai_viewer/__main__.py
+++ rerun_py/depthai_viewer/__main__.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import signal
 import subprocess
 import sys
 import traceback
@@ -9,37 +10,54 @@ import traceback
 from depthai_viewer import bindings, unregister_shutdown
 from depthai_viewer import version as depthai_viewer_version  # type: ignore[attr-defined]
 
+script_path = os.path.dirname(os.path.abspath(__file__))
+venv_dir = os.path.join(script_path, "venv-" + depthai_viewer_version())
+
+
+def delete_partially_created_venv(path: str) -> None:
+    try:
+        if os.path.exists(path):
+            print(f"Deleting partially created virtual environment: {path}")
+            shutil.rmtree(path)
+    except Exception as e:
+        print(f"Error occurred while attempting to delete the virtual environment: {e}")
+        print(traceback.format_exc())
+
+
+def sigint_mid_venv_install_handler(signum, frame) -> None:  # type: ignore[no-untyped-def]
+    delete_partially_created_venv(venv_dir)
+
 
 def create_venv_and_install_dependencies() -> str:
-    script_path = os.path.dirname(os.path.abspath(__file__))
-    venv_dir = os.path.join(script_path, "venv-" + depthai_viewer_version())
-    pip_executable = (
-        os.path.join(venv_dir, "Scripts", "pip") if sys.platform == "win32" else os.path.join(venv_dir, "bin", "pip")
-    )
     py_executable = (
         os.path.join(venv_dir, "Scripts", "python")
         if sys.platform == "win32"
         else os.path.join(venv_dir, "bin", "python")
     )
     try:
+        original_sigint_handler = signal.getsignal(signal.SIGINT)
         # Create venv if it doesn't exist
         if not os.path.exists(venv_dir):
+            # In case of Ctrl+C during the venv creation, delete the partially created venv
+            signal.signal(signal.SIGINT, sigint_mid_venv_install_handler)
             print("Creating virtual environment...")
             subprocess.run([sys.executable, "-m", "venv", venv_dir], check=True)
 
             # Install dependencies
-            subprocess.run([pip_executable, "install", "-U", "pip"], check=True)
+            subprocess.run([py_executable, "-m", "pip", "install", "-U", "pip"], check=True)
             # Install depthai_sdk first, then override depthai version with the one from requirements.txt
             subprocess.run(
                 [
-                    pip_executable,
+                    py_executable,
+                    "-m",
+                    "pip",
                     "install",
                     "git+https://github.com/luxonis/depthai.git@a8dc6e5c26be6d81b8d539bdeb4661087a1d0519#subdirectory=depthai_sdk",
                 ],
                 check=True,
             )
             subprocess.run(
-                [pip_executable, "install", "-r", f"{script_path}/requirements.txt"],
+                [py_executable, "-m", "pip", "install", "-r", f"{script_path}/requirements.txt"],
                 check=True,
             )
 
@@ -59,20 +77,15 @@ def create_venv_and_install_dependencies() -> str:
             print(f"Removing old venv: {item}")
             shutil.rmtree(os.path.join(venv_dir, "..", item))
 
+        # Restore original SIGINT handler
+        signal.signal(signal.SIGINT, original_sigint_handler)
         # Return Python executable within the venv
         return os.path.normpath(venv_packages_dir)
 
     except Exception as e:
         print(f"Error occurred during the creation of the virtual environment or installation of dependencies: {e}")
         print(traceback.format_exc())
-        # Attempt to delete the partially created venv
-        try:
-            if os.path.exists(venv_dir):
-                print(f"Deleting partially created virtual environment: {venv_dir}")
-                shutil.rmtree(venv_dir)
-        except Exception as e:
-            print(f"Error occurred while attempting to delete the virtual environment: {e}")
-            print(traceback.format_exc())
+        delete_partially_created_venv(venv_dir)
         exit(1)
 
 
@@ -81,7 +94,6 @@ def main() -> None:
     python_exe = sys.executable
     # Call the bindings.main using the Python executable in the venv
     unregister_shutdown()
-    print("Using python: ", python_exe, " site-packages: ", venv_site_packages)
     sys.exit(bindings.main(sys.argv, python_exe, venv_site_packages))
 
 
