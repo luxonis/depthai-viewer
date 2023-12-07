3aa06b5b3c39eb0e03337d0d7c35295ff725f9e0
Filip Jeretina <59307111+zrezke@users.noreply.github.com>
2023-12-07 15:13:24 +0100

    OAK D SR POE and some reliability improvements (#35)
    * WIP add support for tof (oak d sr poe)
    
    * Accommodate new sdk, make tof stream show up, more or less properly. Still need to fix a few gui bugs.
    
    * Fixed gui bugs with which sensors are "AI capable" improved a bit on resolution search stability.  did py-lints
    
    * install wheel from artifactory
    
    * Update rerun_py/depthai_viewer/_backend/device.py
    
    * Update rerun_py/depthai_viewer/_backend/device.py

--- rerun_py/depthai_viewer/_backend/device.py
+++ rerun_py/depthai_viewer/_backend/device.py
@@ -6,15 +6,13 @@ from queue import Queue
 from typing import Dict, List, Optional, Tuple
 
 import depthai as dai
+import depthai_viewer as viewer
 import numpy as np
 from depthai_sdk import OakCamera
 from depthai_sdk.classes.packet_handlers import ComponentOutput
 from depthai_sdk.components import CameraComponent, NNComponent, StereoComponent
 from depthai_sdk.components.camera_helper import getClosestIspScale
 from depthai_sdk.components.tof_component import Component, ToFComponent
-from numpy.typing import NDArray
-
-import depthai_viewer as viewer
 from depthai_viewer._backend.device_configuration import (
     ALL_NEURAL_NETWORKS,
     CameraConfiguration,
@@ -52,6 +50,7 @@ from depthai_viewer._backend.packet_handler import (
 )
 from depthai_viewer._backend.store import Store
 from depthai_viewer.install_requirements import model_dir
+from numpy.typing import NDArray
 
 
 class XlinkStatistics:
