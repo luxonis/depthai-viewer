6393c6e4437fda2705bfdd188463760e75086566
zrezke <jeretina.filip@gmail.com>
2024-03-18 14:09:27 +0100

    Fix pylints

--- rerun_py/depthai_viewer/_backend/packet_handler.py
+++ rerun_py/depthai_viewer/_backend/packet_handler.py
@@ -2,7 +2,6 @@ from typing import Callable, List, Optional, Tuple, Union
 
 import cv2
 import depthai as dai
-import depthai_viewer as viewer
 import numpy as np
 from ahrs.filters import Mahony
 from depthai_sdk.classes.packets import (  # PointcloudPacket,
@@ -23,11 +22,13 @@ from depthai_sdk.components import (
     StereoComponent,
 )
 from depthai_sdk.components.tof_component import ToFComponent
+from numpy.typing import NDArray
+from pydantic import BaseModel
+
+import depthai_viewer as viewer
 from depthai_viewer._backend.store import Store
 from depthai_viewer._backend.topic import Topic
 from depthai_viewer.components.rect2d import RectFormat
-from numpy.typing import NDArray
-from pydantic import BaseModel
 
 
 class PacketHandlerContext(BaseModel):  # type: ignore[misc]
