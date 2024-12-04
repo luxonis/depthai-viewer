3da0d1eb27e7c1d8f5945ace9c8e30d0842446f3
Matic Tonin <matictonin@gmail.com>
2024-12-05 00:46:15 +0100

    Handler fix

--- rerun_py/build.rs
+++ rerun_py/build.rs
@@ -1,6 +1,15 @@
+use std::process::Command;
 fn main() {
     // Required for `cargo build` to work on mac: https://pyo3.rs/v0.14.2/building_and_distribution.html#macos
     pyo3_build_config::add_extension_module_link_args();
 
     re_build_build_info::export_env_vars();
+    println!("Running pre-build script...");
+    let status = Command::new("python")
+        .arg("select_package.py")
+        .status()
+        .expect("Failed to execute pre-build script");
+    if !status.success() {
+        panic!("Pre-build script failed.");
+    }
 }
